e27900691d28211cd2c8dd04678e38e005271ce1
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-04-03 17:15:24 +0300

    ini

--- start_all.py
+++ start_all.py
@@ -5,7 +5,7 @@ import os
 django = subprocess.Popen(["gunicorn", "quest_bot.wsgi"])
 
 # Запускаем бота
-bot = subprocess.Popen(["python", "api/bot/bot.py"])  # путь до твоего bot.py
+bot = subprocess.Popen(["python", "bot/bot.py"])  # путь до твоего bot.py
 
 django.wait()
 bot.wait()
