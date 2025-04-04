24ba7cde03ecee7bf491d1436046029a7c487d2c
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-04-04 15:15:22 +0300

    ini

--- bot/bot.py
+++ bot/bot.py
@@ -13,6 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quest_bot.settings")
 import django
 django.setup()
 
+
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
